0df637c24440874fc13c459c55fb5bb80e228ced
inmount <inmount@163.com>
2019-08-19 16:32:18 +0800

    1.1.1908.12

--- SimpleSessionServer/Server/Program.cs
+++ SimpleSessionServer/Server/Program.cs
@@ -19,7 +19,7 @@ namespace Server {
             Console.WriteLine($"[+] 服务启动");
             Console.WriteLine($"[+] 调试状态 {debug}");
             SimpleSessionServer.Server.IsDebug = debug;
-            SimpleSessionServer.Server.Build(IPAddress.Any, "000000", 8601, 60).Run();
+            SimpleSessionServer.Server.Build(IPAddress.Any, "000000", 8601, 3600).Run();
 
         }
     }
